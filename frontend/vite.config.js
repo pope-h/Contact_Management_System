5c3cab6b0a1b9bd350bfacd5818f51e1d4362937
Ekarika Nsemeke <hamatthan@gmail.com>
2023-12-12 23:32:12 -0800

    some hopeful changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Contact_Management_System/",
+  base: "/Contact_Management_System/frontend/",
   plugins: [react()],
 });
